7a27004a4f9e1600dd3ce50025e44165461d6752
Ágoston Babicz <babiczagoston3@gmail.com>
2025-11-30 01:10:26 +0100

    Delete unused functional-test folder

--- functional-test/src/types/player_hand.types.ts
+++
@@ -1,8 +0,0 @@
-import { List } from 'immutable'
-import { Card } from './deck.types'
-
-export type PlayerHand = Readonly<{
-  cards: List<Card>
-  size(): number
-  getPlayerHand(): List<Card>
-}>
